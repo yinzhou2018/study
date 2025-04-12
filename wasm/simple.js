2ec311f885335b86de96391d625ae078641ab3c6
yinzhou <52643161@qq.com>
2025-04-12 17:50:07 +0800

    remove unused projs

--- wasm/simple.js
+++
@@ -1,8 +0,0 @@
-fetch("http://localhost/simple.wasm").then(response => {
-   response.arrayBuffer().then(buffer => {
-      const module = new WebAssembly.Module(buffer);
-      const instance = new WebAssembly.Instance(module);
-      const result = instance.exports.call_add();
-      console.log(result);
-   });
-})
