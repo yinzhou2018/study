65e1d8d371e09fd69c62140e12319f1059da6aa9
yinzhou <52643161@qq.com>
2025-04-28 23:53:45 +0800

    add timer demo

--- rust/playground/src/main.rs
+++ rust/playground/src/main.rs
@@ -1,6 +1,9 @@
-mod algos;
-mod async_await_study;
+// mod algos;
+// mod async_await_study;
+mod timer;
+
 fn main() {
   // algos::algos_study();
-  async_await_study::async_await_study();
+  // async_await_study::async_await_study();
+  timer::timer_study();
 }
